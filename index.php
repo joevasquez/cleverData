8ad41b0bb42f24c274e1f01ddcaede0b0888b4c7
Joe Vasquez <joseph.c.vasquez@gmail.com>
2014-11-20 20:44:18 -0800

    d3 chart

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.3.3/underscore-min.js"></script>
     <script type="text/javascript" src="https://raw.github.com/douglascrockford/JSON-js/master/json2.js"></script>
 	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">    
- 
+	<script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+
     <script type="text/template" id="tpl-html">
 	
 	<style>
@@ -15,6 +16,40 @@
 	td { word-wrap:break-word; }
 	td:nth-child(2), td:nth-child(3), td:nth-child(4) { text-align:center }
 	th:nth-child(2), th:nth-child(3), th:nth-child(4) { text-align:center }
+	
+	.axis path, .axis line { fill: none; stroke: #000; shape-rendering: crispEdges;}
+	.bar { fill: orange; }
+	.bar:hover { fill: orangered ;}
+	.x.axis path { display: none; }
+	
+	.d3-tip {
+	  line-height: 1;
+	  font-weight: bold;
+	  padding: 12px;
+	  background: rgba(0, 0, 0, 0.8);
+	  color: #fff;
+	  border-radius: 2px;
+	}
+	
+	/* Creates a small triangle extender for the tooltip */
+	.d3-tip:after {
+	  box-sizing: border-box;
+	  display: inline;
+	  font-size: 10px;
+	  width: 100%;
+	  line-height: 1;
+	  color: rgba(0, 0, 0, 0.8);
+	  content: "\25BC";
+	  position: absolute;
+	  text-align: center;
+	}
+	
+	/* Style northward tooltips differently */
+	.d3-tip.n:after {
+	  margin: -1px 0 0 0;
+	  top: 100%;
+	  left: 0;
+	}
 	</style>
 
         <div class="well">
@@ -69,7 +104,8 @@
 </head>
 <body style="padding:50px 10px ">
     <div class="container">
-      
+      <div class="chart"></div>
+    
       <h1>Subject Data</h1>
       <table id="tb" class="table">
       <thead>
@@ -190,18 +226,108 @@
 	}
 	$('#tb-2').append(theTable);
 	
-var cell = $('.average');
-
-cell.each(function() {
-    var cell_value = $(this).html();
-    if ((cell_value >= 25) && (cell_value <=40)) {
-        $(this).css({'background' : '#ffff00'});   
-    } else if ((cell_value > 40)) {
-        $(this).css({'background' : '#FF0000'});
-    }
-});	
+	var cell = $('.average');
 	
-		
+	cell.each(function() {
+		var cell_value = $(this).html();
+		if ((cell_value >= 25) && (cell_value <=40)) {
+			$(this).css({'background' : '#ffff00'});   
+		} else if ((cell_value > 40)) {
+			$(this).css({'background' : '#FF0000'});
+		}
+	});	
 	</script>
+    
+    
+	<script src="http://d3js.org/d3.v3.min.js"></script>
+
+<script src="http://labratrevenge.com/d3-tip/javascripts/d3.tip.v0.6.3.js"></script>
+<script>
+
+var margin = {top: 40, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var formatPercent = d3.format(".0%");
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .tickFormat(formatPercent);
+
+var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset([-10, 0])
+  .html(function(d) {
+    return "<strong>average:</strong> <span style='color:red'>" + d.average + "</span>";
+  })
+
+var svg = d3.select(".chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+svg.call(tip);
+
+	// The new data variable.
+	var data = [];
+	for(var j=0;j<subjectData.length;j++){
+		data[j] = {
+			name: subjectData[j].name,
+			average: subjectData[j].average
+		};
+	};
+	
+	console.log("Data: "+data);
+	
+
+
+// The following code was contained in the callback function.
+x.domain(data.map(function(d) { return d.name; }));
+y.domain([0, d3.max(data, function(d) { return d.average; })]);
+
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+  .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("average");
+
+svg.selectAll(".bar")
+    .data(data)
+  .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function(d) { return x(d.name); })
+    .attr("width", x.rangeBand())
+    .attr("y", function(d) { return y(d.average); })
+    .attr("height", function(d) { return height - y(d.average); })
+    .on('mouseover', tip.show)
+    .on('mouseout', tip.hide)
+
+function type(d) {
+  d.average = +d.average;
+  return d;
+}
+
+</script>
 
 </html>
